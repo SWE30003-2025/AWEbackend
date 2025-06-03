0bbbe048e047bec46eefc725461169b24d17bb18
Dan Ngo <danngo.au@gmail.com>
2025-06-04 09:48:06 +1000

    remove date fieds in product table

--- base/models/product_model.py
+++ base/models/product_model.py
@@ -7,8 +7,6 @@ class ProductModel(models.Model):
     description = models.CharField(max_length=255)
     price       = models.DecimalField(max_digits=10, decimal_places=2)
     stock       = models.PositiveIntegerField(default=0)
-    created_at  = models.DateTimeField(auto_now_add=True)
-    updated_at  = models.DateTimeField(auto_now=True)
     category    = models.ForeignKey(
         "base.CategoryModel",
         on_delete=models.SET_NULL,
